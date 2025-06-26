aa3a90e1a73b3bbf56bf5c781bb009e2fc447697
Anushka Gupta <anushkagupta1865@gmail.com>
2025-06-26 12:24:05 +0530

    updated

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd 
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
